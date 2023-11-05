beb91b02808272db584dd18f1bf13e5fbf4114b9
EmreBeratKR <emreberatkrn@gmail.com>
2023-11-05 15:40:18 +0300

    Bisection method

---
+++ Subjects/Numerical Methods/NonLinearSolver.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Numerical_Methods;
+
+public static class NonLinearSolver
+{
+    public static double Bisection(IFunction function, double a, double b, double maxError)
+    {
+        if (function.Evaluate(a) * function.Evaluate(b) >= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(a), nameof(b), $"Bisection method won't be applied in range [{a},{b}].");
+        }
+        
+        var i = 0;
+        
+        while (true)
+        {
+            var c = (a + b) * 0.5;
+            var value = function.Evaluate(c);
+            var error = (b - a) / Math.Pow(2, i);
+            
+            Console.WriteLine($"[{i}]: {c}");
+            if (Math.Abs(error) < maxError) return c;
+
+            i += 1;
+
+            var fA = function.Evaluate(a);
+
+            if (value * fA < 0)
+            {
+                b = c;
+            }
+
+            else
+            {
+                a = c;
+            }
+        }
+    }
+}
--- Subjects/Numerical Methods/Program.cs
+++ Subjects/Numerical Methods/Program.cs
@@ -6,6 +6,17 @@ namespace Numerical_Methods
     {
         public static void Main(string[] args)
         {
+            var a = new PowerFunction(3);
+            var b = new MultiplyFunction(new ConstantFunction(7), new PowerFunction(2));
+            var c = new LinearFunction(14, 0);
+            var d = new ConstantFunction(6);
+            var f = new AddFunction(new SubtractFunction(a, b), new SubtractFunction(c, d));
+            var approximateX = NonLinearSolver.Bisection(f, 0, 1, 1E-50);
+            
+            Console.WriteLine($"[f(x) = {f}] at [x = {approximateX}, y = {f.Evaluate(approximateX)}]");
+            
+            return;
+            
             var sine = new TaylorSeries(new SineFunction(), 0);
             var cosine = new TaylorSeries(new CosineFunction(), 0);
             var euler = new MaclaurinSeries(new NaturalExponentialFunction());

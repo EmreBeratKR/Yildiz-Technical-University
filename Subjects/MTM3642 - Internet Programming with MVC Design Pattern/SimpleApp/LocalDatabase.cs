b7fd25137c21bcc5da869ab55c12c11eb822cc77
EmreBeratKR <emreberatkrn@gmail.com>
2024-04-19 00:20:54 +0300

    save and load localdatabase from json file

--- Subjects/MTM3642 - Internet Programming with MVC Design Pattern/SimpleApp/LocalDatabase.cs
+++ Subjects/MTM3642 - Internet Programming with MVC Design Pattern/SimpleApp/LocalDatabase.cs
@@ -1,48 +1,45 @@
 ﻿using SimpleApp.Models;
-using System;
+using System.Text.Json;
 
 namespace SimpleApp
 {
     public static class LocalDatabase
     {
-        private static List<Student> ms_Students;
-        private static int ms_StudentIdCounter;
+        private const string SAVE_PATH = "local_database_save.json";
+
+
+        private static SaveData ms_Data;
 
 
         public static void Init()
         {
-            ms_Students = new List<Student>();
-
-            AddStudent(new Student()
-            {
-                Id = 0,
-                FirstName = "Emre Berat",
-                LastName = "Kıran",
-                Email = "emre@gmail.com"
-            });
+            Load();
         }
 
         public static void AddStudent(Student student)
         {
-            student.Id = ms_StudentIdCounter++;
-            ms_Students.Add(student);
+            student.Id = ms_Data.StudentIdCounter++;
+            ms_Data.Students.Add(student);
+            Save();
         }
 
         public static Student GetStudentByID(int id)
         {
-            return ms_Students.FirstOrDefault(e => e.Id == id);
+            return ms_Data.Students.FirstOrDefault(e => e.Id == id);
         }
 
         public static bool TryDeleteStudentByID(int id)
         {
+            var students = ms_Data.Students;
             var index = -1;
 
-            for (int i = 0; i < ms_Students.Count; i++)
+            for (int i = 0; i < students.Count; i++)
             {
-                if (ms_Students[i].Id != id) continue;
+                if (students[i].Id != id) continue;
 
-                index = i; 
-                ms_Students.RemoveAt(i);
+                index = i;
+                students.RemoveAt(i);
+                Save();
                 break;
             }
 
@@ -51,11 +48,14 @@ namespace SimpleApp
 
         public static bool TryUpdateStudent(Student student)
         {
-            for (int i = 0; i < ms_Students.Count; i++)
-            {
-                if (ms_Students[i].Id != student.Id) continue;
+            var students = ms_Data.Students;
 
-                ms_Students[i] = student;
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].Id != student.Id) continue;
+
+                students[i] = student;
+                Save();
                 return true;
             }
 
@@ -64,12 +64,42 @@ namespace SimpleApp
 
         public static void DeleteAllStudents()
         {
-            ms_Students.Clear();
+            ms_Data.Students.Clear();
+            Save();
         }
 
         public static IReadOnlyList<Student> GetStudents()
         {
-            return ms_Students;
+            return ms_Data.Students;
+        }
+
+
+        private static void Load()
+        {
+            if (!File.Exists(SAVE_PATH))
+            {
+                ms_Data = new SaveData();
+                ms_Data.Students = new List<Student>();
+                ms_Data.StudentIdCounter = 0;
+                return;
+            }
+
+            var jsonString = File.ReadAllText(SAVE_PATH);
+
+            ms_Data = JsonSerializer.Deserialize<SaveData>(jsonString);
+        }
+
+        private static void Save()
+        {
+            var jsonString = JsonSerializer.Serialize(ms_Data);
+
+            File.WriteAllText(SAVE_PATH, jsonString);
+        }
+
+        private class SaveData
+        {
+            public List<Student> Students { get; set; }
+            public int StudentIdCounter { get; set; }
         }
     }
 }
